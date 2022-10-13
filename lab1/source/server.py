8bed23706d38f20f070da4bb5f296f0368ac3efc
S-T-A-L-K-R <name.generate@gmail.com>
2022-10-13 13:09:13 +0200

    wstępna wersja

--- lab1/source/server.py
+++ lab1/source/server.py
@@ -11,13 +11,16 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
-        
+        now = dt.now()
+        s = now.strftime("%H:%M:%S")
+        t = s.encode()
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()            
             self.wfile.write(b"Hello World!\n")
+            self.wfile.write(t)
         else:
             super().do_GET()
     
