02af21657b5e6024ba5fd502477f0e40673f4075
S-T-A-L-K-R <name.generate@gmail.com>
2022-10-27 12:36:05 +0200

    sprawdźmy urlparse

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+from urlparse import urlparse
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -10,7 +11,10 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
-        
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
+        cmd = query_components["cmd"]
+        print(cmd)
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
