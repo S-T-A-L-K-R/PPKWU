3760cde18acaa6b5fc8383bd6d47ddf1ceb8b6c2
S-T-A-L-K-R <name.generate@gmail.com>
2022-11-10 13:03:21 +0100

    dict2json

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+import json
 
 from datetime import datetime as dt
 from urllib.parse import urlparse
@@ -38,7 +39,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()
             # self.wfile.write(b"Hello World!\n")
-            self.wfile.write(response)
+            self.wfile.write(json.dumps(response))
         else:
             super().do_GET()
     
