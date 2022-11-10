e47f7daa9144d5975eff132391828e0ab4b01ad5
S-T-A-L-K-R <name.generate@gmail.com>
2022-11-10 12:25:37 +0100

    lab2 -> lab3

--- lab3/source/server.py
+++ lab3/source/server.py
@@ -8,15 +8,35 @@ import os
 class web_server(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
-
+    
         print(self.path)
-        
+        parser = urlparse(self.path)
+        self.path = parser.path
+        query = parser.query
+        cmd = ''
+        if query != '':
+            query_components = dict(qc.split("=") for qc in query.split("&"))
+            cmd = query_components.get("cmd")
+            print(cmd)
+            if cmd == "rev":
+                str = query_components.get("str")
+            
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.end_headers()
+            if cmd == "time":
+                now = dt.now(pytz.timezone("Europe/Warsaw"))
+                s = now.strftime("%H:%M:%S") + "\n"
+                t = s.encode() 
+                self.wfile.write(t)
+            elif cmd == "rev":
+                str = str[::-1]
+                str = str.encode()
+                self.wfile.write(str)
+            else:
+                self.wfile.write(b"Hello World!\n")
         else:
             super().do_GET()
     
