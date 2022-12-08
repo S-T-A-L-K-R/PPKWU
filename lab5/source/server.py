9db9b83d79ef8c77b664aae1ebb4efd297d1e6e4
S-T-A-L-K-R <name.generate@gmail.com>
2022-12-08 12:43:20 +0100

    lab4 -> lab5

---
+++ lab5/source/server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+import json
+
+#print('source code for "http.server":', http.server.__file__)
+
+from datetime import datetime as dt
+from urllib.parse import urlparse
+import pytz
+
+class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        parser = urlparse(self.path)
+        self.path = parser.path
+        query = parser.query
+        cmd = ''
+        if query != '':
+            query_components = dict(qc.split("=") for qc in query.split("&"))
+            num1 = int(query_components.get("num1"))
+            num2 = int(query_components.get("num2"))
+            print(num1)
+            print(num2)
+            
+        response =  {"sum" : 0, 
+                     "sub" : 0,
+                     "mul" : 0,
+                     "div" : 0,
+                     "mod" : 0}
+
+        response["sum"] = num1 + num2
+        response["sub"] = num1 - num2
+        response["mul"] = num1 * num2
+        response["div"] = int(num1 / num2)
+        response["mod"] = num1 % num2
+        
+        if self.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()
+            # self.wfile.write(b"Hello World!\n")
+            self.wfile.write(json.dumps(response).encode())
+        else:
+            super().do_GET()
+    
+# --- main ---
+
+PORT = 4080
+
+print(f'Starting: http://localhost:{PORT}')
+
+tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server.serve_forever()
