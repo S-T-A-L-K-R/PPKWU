ff233c984792e9ee28e8d3e3f1b75410a52db5c2
S-T-A-L-K-R <name.generate@gmail.com>
2022-12-08 13:43:38 +0100

    teraz na pewno git

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -17,40 +17,43 @@ class web_server(http.server.SimpleHTTPRequestHandler):
         print(self.path)
         parser = urlparse(self.path)
         self.path = parser.path
-        content_length = int(self.headers['Content-Length'])
-        post_data = json.loads(self.rfile.read(content_length).decode())
-        if post_data != None:
-            response = {}
-            str = post_data.get("str")
-            num1 = int(post_data.get("num1"))
-            num2 = int(post_data.get("num2"))
-            print(str)
-            print(num1)
-            print(num2)
-            if str != None:
-                response["lowercase"] = 0 
-                response["uppercase"] = 0
-                response["digits"] = 0
-                response["special"] = 0
-
-
-                response["lowercase"] = sum(1 for c in str if c.islower())
-                response["uppercase"] = sum(1 for c in str if c.isupper())
-                response["digits"] = sum(1 for c in str if c.isnumeric())
-                response["special"] = len(str) - response["lowercase"] - response["uppercase"] - response["digits"]
+        response = {}
+        content_length = self.headers['Content-Length']
+        if content_length is not None:
+            post_data = json.loads(self.rfile.read(int(content_length)).decode())
+            if post_data != None:
                 
-            if num1 != None and num2 != None:
-                response["sum"] = 0 
-                response["sub"] = 0
-                response["mul"] = 0
-                response["div"] = 0
-                response["mod"] = 0
+                str = post_data.get("str")
+                num1 = post_data.get("num1")
+                num2 = post_data.get("num2")
+                print(str)
+                print(num1)
+                print(num2)
+                if str != None:
+                    response["lowercase"] = 0 
+                    response["uppercase"] = 0
+                    response["digits"] = 0
+                    response["special"] = 0
 
-                response["sum"] = num1 + num2
-                response["sub"] = num1 - num2
-                response["mul"] = num1 * num2
-                response["div"] = int(num1 / num2)
-                response["mod"] = num1 % num2
+                    response["lowercase"] = sum(1 for c in str if c.islower())
+                    response["uppercase"] = sum(1 for c in str if c.isupper())
+                    response["digits"] = sum(1 for c in str if c.isnumeric())
+                    response["special"] = len(str) - response["lowercase"] - response["uppercase"] - response["digits"]
+                    
+                if num1 != None and num2 != None:
+                    num1 = int(num1)
+                    num2 = int(num2)
+                    response["sum"] = 0 
+                    response["sub"] = 0
+                    response["mul"] = 0
+                    response["div"] = 0
+                    response["mod"] = 0
+
+                    response["sum"] = num1 + num2
+                    response["sub"] = num1 - num2
+                    response["mul"] = num1 * num2
+                    response["div"] = int(num1 / num2)
+                    response["mod"] = num1 % num2
         
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
